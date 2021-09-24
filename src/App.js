f534bff50b6e3906d4f112ea34abe37006f784b6
Adrian Hernandez <adridz48@gmail.com>
2021-09-23 22:46:23 -0500

    Creacion de items y css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./page/homepage/homepage.component.jsx";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
