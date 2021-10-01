aa35216a8bba035e179bbd010df43305c56502e7
Adrian Hernandez <adridz48@gmail.com>
2021-09-30 22:53:05 -0500

    Creacion de hat page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {withRouter} from 'react-router-dom';
+
 import "../menu-item/menu-item.styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => {
+export const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => {
   return (
-    <div className= {`${size} menu-item`}>
+    <div className= {`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div className='background-image' style={{
       backgroundImage: `url(${imageUrl})`
     }}/>
@@ -15,4 +17,4 @@ export const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/page/homepage/homepage.component.jsx
+++ src/page/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import "./homepage.styles.scss";
 
 const HomePage = () => {
   return (
-    <div className="homepage">
+    <div className='homepage'>
       <Directory />
     </div>
   );
